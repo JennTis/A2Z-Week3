28fe02b60f1ef266233a73be896a9948925965ee
JennTis <jennifer.tis@nyu.edu>
2017-09-25 23:55:42 -0400

    Uploading files

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let emojis;
+
+function setup() {
+  noCanvas();
+  loadJSON('us_president_quotes.json', gotEmoji);
+
+  let button = select('#button');
+  // Demonstrating anonymous function
+  button.mousePressed(function() {
+    let span = createSpan(random(emojis))
+    // Demonstrating chaining
+    span.style('font-size','64px').parent('emojis');
+  });
+}
+
+function gotEmoji(data) {
+  console.log(data);
+  emojis = data.seaEmoji;
+}
